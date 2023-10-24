59db136d8608fcfea47a8332c844a7a9a6b05e8a
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-24 13:12:29 +0100

    display username info in rendered page

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,17 @@
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { SignOutButton } from "@/components/SignOutButton";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+export const UserInfo = async () => {
+  const session = await getServerSession(options);
+
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <Image src={session?.user?.image} priority={false} width={60} height={60} />
+      <div className="font-bold">{session?.user?.name}</div>
+      <div className="text-sm">{session?.user?.email}</div>
+      <SignOutButton />
+    </div>
+  );
+};
