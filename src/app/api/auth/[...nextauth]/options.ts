fcb4519bd4131a326809b100565f87ea77f9243c
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-25 12:45:40 +0100

    add github public_repo permission and get access_token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,31 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+// extend Session interface to use accessToken
+declare module "next-auth" {
+  interface Session {
+    accessToken?: unknown;
+  }
+}
+
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      authorization: { params: { scope: "read:user user:email public_repo" } },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
